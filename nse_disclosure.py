ff74912a9d7db2c5596a04aca5dc11cdb28efeea
Hedgar <ajakzhedgar@gmail.com>
2023-09-26 06:10:05 -0700

    Update nse_disclosure.py

--- nse_disclosure.py
+++ nse_disclosure.py
@@ -1,86 +1,66 @@
+#created by Hedgar 26/09/2021
+
+import json
+import configparser
 import pandas as pd
 import requests
-import json
 from bs4 import BeautifulSoup
-from datetime import datetime
-import configparser
-import requests.exceptions as requests_exception
+from typing import List, Dict, Union
 
+class CorporateDisclosure:
+    def __init__(self):
+        self.config = self.read_config("nseurl.conf")
+        self.url = self.config.get("nseapi", 'url')
+        self.output_data = "./docs/coy_disclosures.json"
+        self.data = self.fetch_data()
 
-#  get resource
-parser = configparser.ConfigParser(interpolation=None)
-parser.read("nseurl.conf")
-url = parser.get("nseapi", 'url')
+    @staticmethod
+    def read_config(file_path: str) -> configparser.ConfigParser:
+        parser = configparser.ConfigParser(interpolation=None)
+        parser.read(file_path)
+        return parser
 
-output_data = "./docs/coy_disclosures.json"
+    def fetch_data(self) -> List[Dict[str, Union[str, int]]]:
+        try:
+            res = requests.get(self.url)
+            res.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Failed to fetch data: {e}")
+            return []
 
+        soup = BeautifulSoup(res.text, 'xml')
+        entries = soup.find_all('entry')
+        print(f"{len(entries)}, rows of data received")
 
-def corporate_disclosure(url=url):
-    """
-    Issue a GET request and scrape data from xml
-    """
-    # collect data
-    url = str(url)
+        return [{
+            'updated': entry.find('updated').get_text(),
+            'headline': entry.find('Description').get_text(),
+            'location': entry.find('Url').get_text(),
+            'news_class': entry.find('Type_of_Submission').get_text(),
+            'company_name': entry.find('CompanyName').get_text(),
+            'company_symbol': entry.find('CompanySymbol').get_text(),
+            'date_modified': entry.find('Modified').get_text(),
+            'date_created': entry.find('Created').get_text()
+        } for entry in entries]
 
-    nse_company_disclosures = []
-    try:
-        res = requests.get(url)
-        resp = res.text
-    except requests_exception.MissingSchema:
-        print(f"resource locator not available at the moment")
-    except requests_exception.ConnectionError:
-        print(f"Could not connect to resource")
-    soup = BeautifulSoup(resp, 'xml')
-    child = soup.find_all('entry')
-    number_row = len(child)
-    print(f"{number_row}, rows of data received")
-    for siblings in child:
-        grand_children = {}
-        grand_children['updated'] = siblings.find('updated').get_text()
-        grand_children['headline'] = siblings.find('Description').get_text()
-        grand_children['location'] = siblings.find('Url').get_text()
-        grand_children['news_class'] = siblings.find('Type_of_Submission').get_text()
-        grand_children['company_name'] = siblings.find('CompanyName').get_text()
-        grand_children['company_symbol'] = siblings.find('CompanySymbol').get_text()
-        grand_children['date_modified'] = siblings.find('Modified').get_text()
-        grand_children['date_created'] = siblings.find('Created').get_text()
-        nse_company_disclosures.append(grand_children)
-    return nse_company_disclosures
+    def to_json(self) -> None:
+        try:
+            with open(self.output_data, "w") as f:
+                json.dump(self.data, f, indent=4)
+        except Exception as err:
+            print(f"Failed to save to JSON: {err}")
 
+    def save_filtered_data(self, key: str, value: str, file_name: str) -> None:
+        df = pd.DataFrame(self.data)
+        filtered_data = df[df[key] == value]
+        filtered_data.to_csv(f"./docs/{file_name}.csv", index=False)
 
-data = corporate_disclosure()
-
-# save data to json
-
-
-def tojson():
-    try:
-        with open(output_data, "w") as f:
-            json.dump(data, f, indent=4)
-    except Exception as err:
-        print(err)
-
-# save insider dealings notifications to csv
-
-
-def insider_deals():
-    df = pd.DataFrame(data)
-    insider_dealings = df[df['news_class'] == "Directors Dealings"]
-    insider_dealings.to_csv("./docs/insider-dealings.csv", index=False)
-
-
-# save financial statements notifications to csv
-def fin_statement():
-    df = pd.DataFrame(data)
-    fin_statements = df[df['news_class'] == "Financial Statements"]
-    fin_statements.to_csv("./docs/financial-statement.csv", index=False)
-
-
-def main():
-    tojson()
-    insider_deals()
-    fin_statement()
+    def main(self) -> None:
+        self.to_json()
+        self.save_filtered_data('news_class', "Directors Dealings", "insider-dealings")
+        self.save_filtered_data('news_class', "Financial Statements", "financial-statement")
 
 
 if __name__ == "__main__":
-    main()
+    corp_disclosure = CorporateDisclosure()
+    corp_disclosure.main()
